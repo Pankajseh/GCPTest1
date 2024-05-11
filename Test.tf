bd0c387d11a512ca5c1b5d808656f3974387bdb4
Pankajseh <169140784+Pankajseh@users.noreply.github.com>
2024-05-11 17:45:40 +0530

    Create Test.tf

---
+++ Test.tf
@@ -0,0 +1,22 @@
+provider "google" {
+  project = "my-new-project-422716"
+  region  = "us-central1"
+  zone    = "us-central1-a"
+}
+
+resource "random_id" "bucket_suffix" {
+  byte_length = 4
+}
+
+
+
+resource "google_storage_bucket" "dynamic_bucket" {
+  name          = "my-bucket-${random_id.bucket_suffix.hex}"
+  location      = "US"
+  uniform_bucket_level_access = true
+  force_destroy = true
+
+  versioning {
+    enabled = true
+  }
+}
